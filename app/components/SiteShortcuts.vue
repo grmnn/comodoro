963515d13d7a88357acfded91623290e822a0368
grmnn <grmnn@pm.me>
2025-01-18 22:03:24 +0100

    refactor: update text labels in SiteShortcuts component for consistency and clarity

--- app/components/SiteShortcuts.vue
+++ app/components/SiteShortcuts.vue
@@ -1,37 +1,37 @@
 <template>
-	<ul class="text-xs text-zinc-500 flex flex-col gap-y-2">
+	<ul class="text-xs text-zinc-500 flex flex-col gap-y-4">
 		<li class="flex items-center gap-x-2 justify-between">
 			<span><BaseKbd key-label="Space" /></span>
 			<span>Start / Stop timer</span>
 		</li>
 		<li class="flex items-center gap-x-2 justify-between ">
 			<span><BaseKbd key-label="meta" /> <BaseKbd key-label="," /></span>
-			<span>open settings</span>
+			<span>Open Settings</span>
 		</li>
 		<li class="flex items-center gap-x-2 justify-between">
 			<span><BaseKbd key-label="meta" /> <BaseKbd key-label="F" /></span>
-			<span>focus todo input</span>
+			<span>Focus Todo Input</span>
 		</li>
 		<li class="flex items-center gap-x-2 justify-between">
 			<span><BaseKbd key-label="↑" /> <BaseKbd key-label="↓" /></span>
-			<span>navigate between todos</span>
+			<span>Navigate Between Todos</span>
 		</li>
 		<li class="flex items-center gap-x-2 justify-between">
 			<span><BaseKbd key-label="Enter" /></span>
-			<span>toggle todo</span>
+			<span>Toggle Focused Todo</span>
 		</li>
 		<li class="flex items-center gap-x-2 justify-between">
 			<span><BaseKbd key-label="meta" /> <BaseKbd key-label="⌫" /></span>
-			<span>delete todo</span>
+			<span>Delete Focused Todo</span>
 		</li>
 
 		<li class="flex items-center gap-x-2 justify-between">
 			<span><BaseKbd key-label="Alt" /> <BaseKbd key-label="↑" /></span>
-			<span>move todo up</span>
+			<span>Move Focused Todo Up</span>
 		</li>
 		<li class="flex items-center gap-x-2 justify-between">
 			<span><BaseKbd key-label="Alt" /> <BaseKbd key-label="↓" /></span>
-			<span>move todo down</span>
+			<span>Move Focused Todo Down</span>
 		</li>
 	</ul>
 </template>
